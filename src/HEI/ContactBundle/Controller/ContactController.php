7e36fa13dce17d43970accd6776365805c7dbe81
Unknown <v.fuger-harnois@orange.fr>
2018-02-13 11:15:10 +0100

    Cleaning tree

---
+++ src/HEI/ContactBundle/Controller/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 13/02/2018
+ * Time: 10:15
+ */
+
+namespace HEI\ContactBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContactController extends Controller
+{
+    public function indexAction() {
+        return $this->render('HEIContactBundle:Contact:index.html.twig');
+    }
+}
---
+++ src/HEI/ContactBundle/HEIContactBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HEI\ContactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HEIContactBundle extends Bundle
+{
+}
